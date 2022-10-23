762332ebf73e6479d539fe6fc14abb13aa045d08
storres21 <s.torres21@uniandes.edu.co>
2022-10-22 21:12:16 -0500

    cambio index

--- scripts/main.js
+++ scripts/main.js
@@ -9,16 +9,11 @@ function renderSerieInTable(series) {
         var trElement = document.createElement("tr");
         trElement.innerHTML = "<td>".concat(series.id, "</td>\n                             <td style=\"color: DodgerBlue;\">").concat(series.name, "</td>\n                             <td>").concat(series.channel, "</td>\n                             <td>").concat(series.seasons, "</td>");
         seriesTbody.appendChild(trElement);
-        trElement.addEventListener("click", function () { return renderCard(series); });
     });
 }
 function getSeasonsAverage(series) {
     var totalSeasons = 0;
-    dataSeries.forEach(function (dataSerie) { return totalSeasons = totalSeasons + dataSerie.seasons; });
+    dataSeries.forEach(function (series) { return totalSeasons = totalSeasons + series.seasons; });
     var seasonsAverage = totalSeasons / dataSeries.length;
     return seasonsAverage;
-}
-function renderCard(serie) {
-    var card = document.getElementById("idcard");
-    card.innerHTML = "<div class=\"card\" id=\"idcard\">\n    <img src=\"".concat(serie.image, "\" class=\"card-img-top\" alt=\"Image\">\n    <div class=\"card-body\">\n      <h5 class=\"card-title\">").concat(serie.name, "</h5>\n      <p class=\"card-text\">").concat(serie.description, "</p>\n      <a href=\"").concat(serie.link, "\">").concat(serie.link, "</a>\n    </div>\n  </div>");
 }
